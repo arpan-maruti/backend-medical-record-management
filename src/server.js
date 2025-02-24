91844cecaca89992fdb8d52d072627aa4bbe0741
hpreksha <preksha.hirani@marutitech.com>
2025-02-24 12:56:34 +0530

    feat:Added db pooling

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-
 import mongoose from 'mongoose';
 import process from 'process';
 import app from './app.js';
 
 const dbUrl = process.env.MONGO_URI;
+const startTime = new Date();
+const poolOptions = {
+  connectTimeoutMS: 30000,
+  maxConnecting: 5,
+  maxIdleTimeMS: 30000,
+  maxPoolSize: 10,
+  minPoolSize: 5,
+  waitQueueTimeoutMS: 5000
+};
+mongoose.connect(dbUrl, poolOptions)
+.then(() => {
+        console.log('Connected to MongoDB with pooling enabled');
 
-mongoose.connect(dbUrl)
-    .then(() => {
-        console.log('Connected to MongoDB');
     })
     .catch((err) => {
         console.log('MongoDB connection error:', err);
@@ -17,5 +25,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-

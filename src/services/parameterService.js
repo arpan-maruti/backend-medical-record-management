aef06879e68ea7b1f6783cbdea084c485124f38e
hpreksha <preksha.hirani@marutitech.com>
2025-02-25 19:21:24 +0530

    refactor: updated services

--- src/services/parameterService.js
+++ src/services/parameterService.js
@@ -1,25 +1,20 @@
-// fetch details from database and return no extra validations
-// Create: createEntity(), createUser(), addProduct()
-// Read: getEntity(), findUserById(), fetchAllProducts()
-// Update: updateEntity(), updateUser(), modifyProduct()
-// Delete: deleteEntity(), removeUser(), removeProduct()
-// Other common methods: getEntityById(), findEntityByField(), processPayment()
-
 import Parameter from "#models/parameter.js";
 import InstructionType from "#models/instructionType.js";
 export const addParameterService = async({
     instructionId, parameterMsg, significanceLevel, createdBy, modifiedBy
 }) => {
-    const parameter = new Parameter({instructionId, parameterMsg, significanceLevel, createdBy, modifiedBy});
-    await parameter.save();
-    return parameter;
+    try {
+        const parameter = new Parameter({instructionId, parameterMsg, significanceLevel, createdBy, modifiedBy});
+        return await parameter.save({runValidators: true});
+    } catch (err) {
+        throw new Error(err.message);
+    }
 }
 
 export const getParametersByInstructionService = async({id}) => {
-    const instruction = await InstructionType.findById(id);
-    if(!instruction) {
-        throw new Error("instruction not found");
+    try {
+        return await Parameter.find({instructionId:id, isDeleted: false})
+    } catch (err) {
+        throw new Error(err.message);
     }
-    const parameters = await Parameter.find({instructionId:id, isDeleted: false})
-    return parameters;
 }
